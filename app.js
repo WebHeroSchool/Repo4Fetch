15c37dff9cffc25aa499a6d5dc1df5d389b3986f
Alexander Aleshin <hichrov@gmail.com>
2021-03-22 01:42:08 +0300

    testing work with async await

--- app.js
+++ app.js
@@ -3,34 +3,46 @@ const userName = document.querySelector('.user__nickname');
 const userBio = document.querySelector('.user__description');
 const url = window.location.search;
 const params = new URLSearchParams(url);
-console.log(params.get("name"));
-console.log("url", url);
+// console.log(params.get("name"));
+// console.log("url", url);
 
-let nickName = params.get("name");
+let nickName = prompt('Enter Your NickName', 'Icreateacoolname');
 if (!nickName) {
     nickName = 'Icreateacoolname';
 }
 
-async function getResponse(nickName) {
-    const response = await fetch(`https://api.github.com/users/${nickName}`);
-    
-    const content = await response.json();
 
-    let name;
-    let bio;
+// let getnickName = params.get("name");
+
+const delay = ms => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  })
+}
+
+async function getResponse() {
+  await delay(2000);
+  let response = await fetch(`https://api.github.com/users/${nickName}`);
+  return response.json();
+} //получает запров в виде json файла
+
+async function getUserData() {
+  try {
+    const content = await getResponse();
 
     if (content.name) {
-        name = userName.append(content.name);
+      userName.append(content.name);
     }else {
-        userName.innerHTML = 'Информация о пользователе недоступна!';
+      userName.append(' ' + 'Информация о пользователе недоступна!');
     }
 
     if (content.bio) {
-        bio = userBio.append(content.bio);
+      userBio.append(content.bio);
     }else {
-        userBio.innerHTML = 'Информация о пользователе недоступна!';
+      userBio.append(' ' + 'Информация о пользователе недоступна!');
     }
-
 
     let photo = new Image(300, 400);
     photo.src = content.avatar_url;
@@ -39,7 +51,17 @@ async function getResponse(nickName) {
     let link = content.html_url;
     userName.onclick = () => window.location.assign(link);
     userName.style.cursor = 'pointer';
+  }
+  catch {
+    console.error(error);
+  }
+}//дожидается конца работы getResponse и работает с данными
+
+async function getDate() {
+  await delay(2000);
+  console.log (new Date());
 }
 
+getUserData();
+getDate();
 
-getResponse(nickName);
