edb5d9184d881d4a751b04cc721bdb50ea7b935c
Alexander Aleshin <hichrov@gmail.com>
2021-03-25 01:23:01 +0300

    it's a miracle...

--- app.js
+++ app.js
@@ -1,80 +1,84 @@
 const userPhoto = document.querySelector('.user__photo');
 const userName = document.querySelector('.user__nickname');
 const userBio = document.querySelector('.user__description');
-const date = document.querySelector('.date');
-// const url = window.location.search;
+const cardDiv = document.querySelector('.card');
+const dateDiv = document.querySelector('.date');
 const url = 'https://api.github.com/users/';
-// const params = new URLSearchParams(url);
 const preloader = document.querySelector('.preloader');
-// console.log(params.get("name"));
-// console.log("url", url);
+const form = document.getElementById('form');
+const usernameInput = document.getElementById("usernameInput");
+let usernameInputValue = usernameInput.value;
+let nickName = usernameInputValue;
 
-window.onload = function stopPreloader() {
-  setTimeout(function () {
-    document.body.classList.add('loaded_hiding');
-    document.body.classList.add('loaded');
-  }, 2000);
+const startPreloader = () => { 
+  document.body.classList.remove('loaded_hiding');
+  document.body.classList.remove('loaded');
+}
+async function stopPreloader() { 
+  await delay(1000);
+  document.body.classList.add('loaded_hiding');
+  document.body.classList.add('loaded');
 }
 
-const input = document.getElementById('input');
-
-input.onkeyup = async function() {
-  let nickName =  input.value;
-  getResponse(nickName);
+const closeInputForm = () => {
+    cardDiv.classList.remove('hidden');
+    form.classList.remove('form');
+    form.classList.add('hidden');
 }
 
-// let getnickName = params.get("name");
+const delay = ms => new Promise((resolve) => setTimeout(() => resolve(), ms));
 
-const delay = ms => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, ms);
-  })
+usernameInput.onchange = (event) => {
+  usernameInputValue = event.target.value;
+  nickName = usernameInputValue; // наверное надо вернуть nickName
 }
 
-
-async function getResponse(nickName) {
-  try { 
-    let response = await fetch(url+nickName);
-    if(response.status >= 400) {
-      throw new Error('не удалось найти такого пользователя');
-    }
-    return response.json();
+async function getResponse() {
+  try {
+      const response = await fetch(`${url}${nickName}`);
+      if (!response.ok) {
+          throw new Error('не удалось найти такого пользователя');
+      }
+      return response.json();
+  } catch (error) {
+      console.log(`Fetch Error: ${error.message} o_O`);
   }
-  catch(e) {
-    console.log(`Fetch Error: ${e.message} o_O`);
-  }
-  
-} //получает запров в виде json файла
-
-async function getUserData() {
-    const content = await getResponse();
-    const{name, bio, avatar_url, html_url} = content;    
-
-    !name ? userName.append(' ' + 'Информация о пользователе недоступна!') : userName.append(name);
-    !bio ? userBio.append(' ' + 'Информация о пользователе недоступна!') : userBio.append(bio);      
-
-    let photo = new Image(300, 400);
-    photo.src = avatar_url;
-    userPhoto.append(photo);
-
-    let link = html_url;
-    userName.onclick = () => window.location.assign(link);
-    userName.style.cursor = 'pointer';
-
-    return {name, bio, avatar_url, html_url};
-}//дожидается конца работы getResponse и работает с данными
+}
 
 async function getDate() {
   await delay(4000);
-  const nowDate = new Date()
-  date.append(nowDate);
-  return nowDate;
+  return new Date();
 }
 
-Promise.all([getUserData(), getDate()])
-  .then(([userData, date]) => {
-    console.log(userData, date);
-  })
-    
+function getUserData(obj) {
+  const {name, bio, avatar_url, html_url} = obj;
+
+  !name ? userName.append(' Информация о пользователе недоступна!') : userName.append(name);
+  !bio ? userBio.append(' Информация о пользователе недоступна!') : userBio.append(bio);
+
+  let photo = new Image(300, 400);
+  photo.src = avatar_url;
+  userPhoto.append(photo);
+
+  let link = html_url;
+  userName.onclick = () => window.location.assign(link);
+  userName.style.cursor = 'pointer';
+
+  return {name, bio, avatar_url, html_url};
+}
+
+stopPreloader();
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  startPreloader();
+  closeInputForm();
+
+
+  Promise.all([getResponse(), getDate()])
+      .then(([response, date]) => {
+          setTimeout(stopPreloader, 2000);
+          dateDiv.append(date);
+          getUserData(response);
+      })
+}
